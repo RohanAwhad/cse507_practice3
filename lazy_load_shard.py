ecaf3bcc4af9f619cd2b5794778f413e6f98762e
Rohan Awhad <rohanavad007@gmail.com>
2024-10-11 16:22:54 -0700

    added lazy loading for sharding

--- lazy_load_shard.py
+++ lazy_load_shard.py
@@ -1,3 +1,75 @@
+import dataclasses
+import json
+import os
+import pandas as pd
+import warnings
+from tqdm import tqdm
+from typing import Callable, Dict, List
+
+import logger
+
+batch_size = 1000
+
+def get_files_with_absolute_paths(root_dir: str, index_name: str) -> dict[str, str]:
+    index_path = f'/home/rawhad/CSE507/practice_3/{index_name}_index.json'
+    if os.path.exists(index_path):
+        with open(index_path, 'r') as f: return json.load(f)
+    file_dict: dict[str, str] = {}
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            absolute_path = os.path.abspath(os.path.join(dirpath, filename))
+            file_dict[filename] = absolute_path
+    with open(index_path, 'w') as f: json.dump(file_dict, f)
+    return file_dict
+chestxray14_index = get_files_with_absolute_paths('/data/courses/2024/class_ImageSummerFall2024_jliang12/chestxray14', 'chestxray14')
+print('Chest XRay 14 index loaded')
+padchest_index = get_files_with_absolute_paths('/data/jliang12/shared/dataset/PadChest/image_zips', 'padchest')
+print('PadChest index loaded')
+
+
+@dataclasses.dataclass
+class DS:
+    name: str
+    csv_path: str
+    col_name: str
+    get_image_path: Callable[[str], str]
+
+csv_directory: str = "/data/courses/2024/class_ImageSummerFall2024_jliang12/CheXMask/physionet.org/files/chexmask-cxr-segmentation-data/0.4/OriginalResolution"
+chestxray14_ds = DS(
+    name="chestxray14",
+    csv_path=os.path.join(csv_directory, "ChestX-Ray8.parquet"),
+    col_name="Image Index",
+    get_image_path=lambda x: chestxray14_index.get(x, None)
+)
+padchest_ds = DS(
+    name="padchest",
+    csv_path=os.path.join(csv_directory, "Padchest.parquet"),
+    col_name="ImageID",
+    get_image_path=lambda x: padchest_index.get(x, None)
+)
+
+def get_image_path(path: str) -> str | None: return path if os.path.exists(path) else None
+chexpert_ds = DS(
+    name="chexpert",
+    csv_path=os.path.join(csv_directory, "CheXpert.parquet"),
+    col_name="Path",
+    get_image_path=lambda x: get_image_path(f"/data/courses/2024/class_ImageSummerFall2024_jliang12/chexpertchestxrays-u20210408/CheXpert-v1.0/{x}")
+)
+
+vindr_cxr_ds = DS(
+    name="vindr_cxr",
+    csv_path=os.path.join(csv_directory, "VinDr-CXR.parquet"),
+    col_name="image_id",
+    get_image_path=lambda x: get_image_path(f"/data/courses/2024/class_ImageSummerFall2024_jliang12/vinbigdata/train/{x}.dicom"),
+)
+
+datasets_dict: Dict[str, DS] = {
+    chestxray14_ds.name: chestxray14_ds,
+    padchest_ds.name: padchest_ds,
+    chexpert_ds.name: chexpert_ds,
+    vindr_cxr_ds.name: vindr_cxr_ds,
+}
+
 import pandas as pd
 import numpy as np
 import os
